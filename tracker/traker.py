d7e9ea4f290f5457cce04a1d8ba9e9156a7c1dda
Punit Soni <punitxsmart@gmail.com>
2014-04-26 17:11:00 -0700

    tracker: add gradient and velocity support

--- tracker/traker.py
+++ tracker/traker.py
@@ -3,6 +3,7 @@
 import cv2
 import numpy as np
 from keycodes import *
+import time
 
 def nothing(x):
   pass
@@ -15,21 +16,24 @@ class TargetTracker:
   _width = 320
   _height = 240
   _debugMode = False
-  _shotNum = 0;
+  _shotNum = 0
+  _posCb = None
+  _prevPos = (0, 0)
 
-  def __init__(self):
+  def __init__(self, updatePosCb=None):
     cv2.namedWindow(self._winTracker)
     cv2.moveWindow(self._winTracker, 10, 10)
-    cv2.createTrackbar(self._barHval, self._winTracker, 100, 180, nothing)
-    cv2.createTrackbar("H max", self._winTracker, 120, 180, nothing)
+    cv2.createTrackbar(self._barHval, self._winTracker, 120, 180, nothing)
 
     self._capture = cv2.VideoCapture(-1)
     self._capture.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, self._width)
     self._capture.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, self._height)
+    self._posCb = updatePosCb
 
   def initDebugMode(self):
     cv2.namedWindow(self._winMask)
     cv2.moveWindow(self._winMask, 10, self._height+50)
+
   def deinitDebugMode(self):
     cv2.destroyWindow(self._winMask)
 
@@ -40,11 +44,12 @@ class TargetTracker:
       frame = np.fliplr(frame).copy()
       self.processImage(frame)
       cv2.imshow(self._winTracker, frame)
-      #cv2.imshow(self._winMask, mask)
       key = cv2.waitKey(10)
 
       if key == KEY_ESC:
         break;
+      if key == KEY_ALPHA('d'):
+        self._debugMode = not self._debugMode;
       elif key == KEY_ALPHA('p'):
         while True:
           key1 = cv2.waitKey(10)
@@ -62,18 +67,17 @@ class TargetTracker:
   def processImage(self, frame):
     blur = cv2.GaussianBlur(frame, (7,7), 0)
     hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
-    Hmin = cv2.getTrackbarPos(self._barHval, self._winTracker)
-    Hmax = cv2.getTrackbarPos("H max", self._winTracker)
+    Hval = cv2.getTrackbarPos(self._barHval, self._winTracker)
 
-    blue_min = np.array([Hmin-10, 50, 20], np.uint8)
-    blue_max = np.array([Hmin+10, 255, 255], np.uint8)
+    blue_min = np.array([Hval-10, 50, 20], np.uint8)
+    blue_max = np.array([Hval+10, 255, 255], np.uint8)
 
     mask = cv2.inRange(hsv, blue_min, blue_max)
     kernel = np.ones((3,3),np.uint8)
     mask = cv2.dilate(mask, kernel, iterations=1)
     if self._debugMode == True:
       self.initDebugMode()
-      imshow(self._winMask, mask)
+      cv2.imshow(self._winMask, mask)
     else:
       self.deinitDebugMode()
 
@@ -87,30 +91,41 @@ class TargetTracker:
         maxArea = area
         largestCnt = cnt
 
-    print maxArea
+    #print maxArea
     if maxArea > 100.0:
       M = cv2.moments(largestCnt)
       cx = int(M['m10']/M['m00'])
       cy = int(M['m01']/M['m00'])
-      print "center:", cx, cy
-      cv2.circle(frame, (cx, cy), 3, (0, 0, 255))
+      pos = (cx, cy)
+      #print "center:", cx, cy
+      cv2.circle(frame, pos, 3, (0, 0, 255))
       cv2.drawContours(frame, [largestCnt], 0, (0,255,0), 1)
+      grad = (pos[0] - self._prevPos[0], pos[1] - self._prevPos[1])
+      self._prevPos = pos
+      if self._posCb is not None:
+        self._posCb(pos, grad)
 
+    # Draw overlays
     c = np.zeros((1, 1, 3), np.uint8)
-    c[0, 0, :] = [Hmin, 255, 255]
+    c[0, 0, :] = [Hval, 255, 255]
     c = cv2.cvtColor(c, cv2.COLOR_HSV2BGR)
-
     cv2.rectangle(frame, (0, 0), (20, 20), tuple(c[0, 0].tolist()), -1)
-
-    '''rect = cv2.minAreaRect(largestCnt)
-    box = cv2.boxPoints(rect)
-    box = np.int0(box), 3
-    frame = cv2.drawContours(frame,[box],0,(0,0,255),2)'''
 # end of TargetTracker
 
 # main
+prevTime = 0
+def posCb(pos, grad):
+  global prevTime
+  curTime = time.time()
+  dt = curTime-prevTime
+  prevTime = curTime
+  (x, y) = pos;
+  (dx, dy) = grad
+  print "pos, grad =  (%d, %d), (%d, %d), v = (%f, %f)" \
+     % (x, y, dx, dy, dx/dt, dy/dt)
+
 if __name__ == "__main__":
-  tracker = TargetTracker()
+  tracker = TargetTracker(posCb)
   tracker.run()
   tracker.cleanup()
   print "finished."
